6179eb7031eb8dbd7d31a10fda7283e231a65ecb
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-12-08 18:08:16 +0300

    Change it to vacation form validation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import "./Header.css";
 function Header() {
   return (
     <div>
-      <h1 className="title">Register</h1>
+      <h1 className="title">Vakansiya yaratish</h1>
+      <p>Yangi vakansiya uchun ma’lumotlar</p>
     </div>
   );
 }
